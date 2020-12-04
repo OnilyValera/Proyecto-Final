49b5bdb90e50f1d55a9198b3b88a41d6b7ed46e0
Onily Valera <70966644+OnilyValera@users.noreply.github.com>
2020-12-03 23:52:02 -0400

    Add files via upload

---
+++ ITLA/Controllers/AlumnoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITLA.Infraestructured.Repositorio;
+
+
+namespace ITLA.Api.Controllers
+{
+    public class AlumnoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAlumno()
+        {
+            var alumno = new AlumnoRepositorio().GetAlumno();
+            return Ok(alumno);
+        }
+
+    }
+
+
+}
---
+++ ITLA/Controllers/ComentarioController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITLA.Infraestructured.Repositorio;
+
+namespace ITLA.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentarioController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetComentario()
+        {
+            var comentario = new ComentarioRepositorio().GetComentario();
+            return Ok(comentario);
+        }
+
+    }
+}
